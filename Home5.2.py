8eec9358ad9af98b8000372662ae893c25443bb0
School47 <School47@gmail.com>
2023-06-03 13:11:18 +0500

    2

--- Home5.2.py
+++ Home5.2.py
@@ -4,3 +4,14 @@
 # <function_name>(0,0) -> 0
 # <function_name>(0,2) -> 2
 # <function_name>(3,0) -> 3
+
+def sum(a, b):
+    if b == 0:
+        return a
+    else:
+        return sum(a+1, b-1)
+    
+a = int(input("Введите а:"))
+b =  int(input("Введите b:"))
+result = sum(a, b)
+print(result)
